2bdf694310e5074010e564d392d72ec18feb077a
RAJHIMANSHUPATEL <blazehimanshuraj@gmail.com>
2024-05-15 09:48:52 +0530

    zod validation

--- server/router/auth-router.js
+++ server/router/auth-router.js
@@ -1,9 +1,11 @@
 const express = require('express');
 const router = express.Router();
-const authControllers = require('../controllers/auth-controller.js')
+const authControllers = require('../controllers/auth-controller.js');
+const validate = require("../middlewares/validate-middleware.js");
+const signUpSchema = require('../validators/auth-validator.js');
 
 router.route("/").get(authControllers.home);
-router.route("/register").post(authControllers.register);
+router.route("/register").post(validate(signUpSchema) ,authControllers.register);
 router.route("/login").post(authControllers.login);
 
 
